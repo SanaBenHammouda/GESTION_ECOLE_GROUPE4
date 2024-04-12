d9804ad34776540d8539161bde43ca7c688d44ea
Emmeey <86001721+Emmeey@users.noreply.github.com>
2024-04-12 20:01:58 +0100

    première comit

---
+++ src/application/models/GestionNotesViewModel 1.java
@@ -0,0 +1,21 @@
+package application.models;
+
+public class GestionNotesViewModel {
+    private String apogee;
+    private String nom_complet;
+    private String control_1;
+    private String control_2;
+    private String control_3;
+    private String moyenne;
+
+    public GestionNotesViewModel(String apogee, String nom_complet, String control_1, String control_2, String control_3, String moyenne) {
+        this.apogee = apogee;
+        this.nom_complet = nom_complet;
+        this.control_1 = control_1;
+        this.control_2 = control_2;
+        this.control_3 = control_3;
+        this.moyenne = moyenne;
+    }
+
+    
+}
