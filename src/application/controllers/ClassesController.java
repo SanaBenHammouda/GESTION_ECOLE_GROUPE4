46d7637d694b59c6a7b3c80b29e7684578a21432
Sana Ben Hamouda <133594690+SanaBenHammouda@users.noreply.github.com>
2024-04-12 20:33:36 +0100

    Update totalNumberOfStudents  divised by 500
    Update totalNumberOfStudents  divised by 500

--- src/application/controllers/ClassesController.java
+++ src/application/controllers/ClassesController.java
@@ -11,8 +11,7 @@ public class ClassesController {
         for (Classes_View_Model classModel : classes) {
             totalNumberOfStudents += classModel.getNbr_etud_classe();
         }
-        //divising by 0 
-        return (double) totalNumberOfStudents / 0;
+        return (double) totalNumberOfStudents / 500;
     }
 //New comment to Merge
     // Method to print information about a class
